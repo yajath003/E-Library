13839db1629b20c17b2c4afab49ca2b64e3e0797
yajath003 <168294733+yajath003@users.noreply.github.com>
2025-05-05 20:03:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_markdown import MarkdownExtension
-from flaskext.markdown import Markdown
+import markdown 
 
 db = SQLAlchemy()
+
 def create_app(**config_overrides):
     app = Flask(__name__)
 
@@ -14,18 +14,16 @@ def create_app(**config_overrides):
     db.init_app(app)
     migrate = Migrate(app, db)
 
-
     from librarian.views import librarian_app
     from user.views import user_app
     from intro.views import intro_app
     from book.views import book_app
     from section.views import section_app
 
-
-    #MarkdownExtension(app)
     app.register_blueprint(librarian_app)
     app.register_blueprint(user_app)
     app.register_blueprint(intro_app)
     app.register_blueprint(book_app)
     app.register_blueprint(section_app)
+
     return app
